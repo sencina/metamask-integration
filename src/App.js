10508fe1f93be604cd47d7a140fe2f7848c7741b
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-05-20 12:28:45 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import WalletCard from "./components/WalletCard/WalletCard";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
       <div className={'App'}>
-            <WalletCard/>
+            <Button/>
       </div>
   );
 }
