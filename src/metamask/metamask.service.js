3ff5a77b7e8a773134c17a4f1acb1d7df1f07d48
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-05-28 12:56:43 -0300

    deployment with service

--- src/metamask/metamask.service.js
+++ src/metamask/metamask.service.js
@@ -1,6 +1,52 @@
 import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
 
 export class MetamaskService{
 
+    getProvider = async () => {
+        return await detectEthereumProvider();
+    }
 
+    hasProvider = () => {
+        return window.ethereum !== undefined;
+    }
+
+    enable = async () => {
+        return await window.ethereum.enable();
+    }
+
+    connectWallet = async () => {
+        const provider = await this.getProvider();
+        const accounts = await provider.request({method: 'eth_requestAccounts'});
+        return accounts[0];
+    }
+
+    deploy = async (data, args, sender) => {
+
+        const provider = await this.getProvider();
+        const web3 = new Web3(provider);
+        const contract = new web3.eth.Contract(data.abi);
+        const deployerContract = await contract.deploy({
+            data: data.bytecode,
+            arguments: args
+        }).send({
+            from: sender,
+            gas: '5000000',
+        })
+
+        return deployerContract.options.address;
+
+    }
+
+    approve = async (token, spender, amount, sender) => {
+
+        const provider = await this.getProvider();
+        const web3 = new Web3(provider);
+        const contract = new web3.eth.Contract(token.abi, token.address);
+        const approve = await contract.methods.approve(spender, amount).send({
+            from: sender,
+            gas: '5000000',
+        })
+        return approve;
+    }
 }
